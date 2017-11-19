0972a333ce79531e0e199fdf458e60930d80fd17
Tyler Stanley <mr.tylerstanley@gmail.com>
2017-11-18 18:13:48 -0700

    initial commit

---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<Movie> movies;
+        public SelectList genres;
+        public SelectList releaseDate;
+        public string movieGenre { get; set; }
+        public string sortOrder { get; set; }
+    }
+}
